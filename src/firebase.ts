41b12542c27f4ba1b4f2c9b7f829e5929ae8c444
hyoloui <tmdgy9272@gmail.com>
2023-09-18 00:29:43 +0900

    init: Firebase setup
    - Auth, DB 서비스 이용을 위한 파이어베이스를 연동합니다

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "nextjs-chat-aee05.firebaseapp.com",
+  projectId: "nextjs-chat-aee05",
+  storageBucket: "nextjs-chat-aee05.appspot.com",
+  messagingSenderId: "906656669094",
+  appId: "1:906656669094:web:2e54621036101e8e000367",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+
+export default app;
