ae8ff69b140827b57450e9059ac1b939b4229b0a
hyoloui <tmdgy9272@gmail.com>
2023-09-19 22:18:34 +0900

    feat: Login 상태 별 rendering 분기처리
    - React-firebase-hooks의 useAuthState를 활용해 로그인 상태를 감지합니다
    - 로딩중이라면 spinner, 비로그인이라면 Login, 로그인이라면 정상렌더링

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { auth } from "@/firebase";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { IoChatbubbleOutline } from "react-icons/io5";
+import { CgSpinner } from "react-icons/cg";
+
+import SideBar from "@/components/SideBar";
+import Login from "@/components/Login";
+
 export default function Home() {
-  return <main className="grid w-full grid-cols-8"></main>;
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center w-full h-screen">
+        <CgSpinner className="animate-spin w-10 h-10" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Login />;
+  }
+  return (
+    <main className="grid w-full grid-cols-8">
+      <div className="col-span-2">
+        <SideBar />
+      </div>
+
+      <div className="col-span-6 flex justify-center h-screen">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <IoChatbubbleOutline className="w-24 h-24 text-gray-300" />
+          <p className="text-2xl text-gray-300">대화를 시작합니다.</p>
+        </div>
+      </div>
+    </main>
+  );
 }
