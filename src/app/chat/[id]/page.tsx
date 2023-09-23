5ace9d0980c6d686343bf9394f2d8b2495bf7409
hyoloui <tmdgy9272@gmail.com>
2023-09-24 01:39:19 +0900

    feat: 채팅 입력 Form 생성 ( BottomBar )
    - 하단에 채팅을 입력할 수 있는 form 을 생성합니다

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,18 +1,34 @@
 "use client";
 
+import { auth } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { useParams } from "next/navigation";
+
+import BottomBar from "@/components/BottomBar";
 import SideBar from "@/components/SideBar";
 
+import type { User } from "firebase/auth";
+
 const ChatPage = () => {
+  const { id } = useParams();
+  const [user] = useAuthState(auth);
+
   return (
     <main className="grid w-full grid-cols-8">
       <div className="col-span-2">
         <SideBar />
       </div>
 
-      <div className="col-span-6 flex justify-center h-screen">
-        <div className="flex flex-col items-center justify-center space-y-4">
-          initail page
+      <div className="flex flex-col w-full col-span-6">
+        {/* Top bar */}
+        Top bar
+        <div className="flex w-full h-full px-6 pt-4 mb-2 overflow-y-scroll no-scrollbar">
+          <div className="flex flex-col w-full">{/* Message */}</div>
+          Message
         </div>
+        {/* Bottom bar */}
+        <BottomBar user={user as User} chatId={id as string} />
       </div>
     </main>
   );
--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,7 +1,51 @@
 "use client";
 
-const BottomBar = () => {
-  return <div>BottomBar</div>;
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "@/firebase";
+import type { User } from "firebase/auth";
+
+import { useState } from "react";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+
+interface IBottomBarProps {
+  user: User;
+  chatId: string;
+}
+const BottomBar = ({ user, chatId }: IBottomBarProps) => {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input.trim() === "") return;
+
+    await addDoc(collection(db, `chats/${chatId}/messages`), {
+      text: input,
+      sender: user.email,
+      photoURL: user.photoURL,
+      timestamp: serverTimestamp(),
+    });
+
+    setInput("");
+  };
+  return (
+    <div>
+      <form
+        onSubmit={sendMessage}
+        className="flex items-end w-full px-6 pb-4 space-x-2"
+      >
+        <input
+          className="w-full px-4 py-4 placeholder-gray-400 border border-gray-300 rounded-lg focus:gray-400"
+          type="text"
+          placeholder="메시지를 입력하세요"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">
+          <IoPaperPlaneOutline className="mb-4 text-gray-600 w-7 h-7 hover:text-gray-900" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default BottomBar;
