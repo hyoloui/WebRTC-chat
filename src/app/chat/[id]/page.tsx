80e94505f82f1f0a0639ca8fcafcc10db51dc935
hyoloui <tmdgy9272@gmail.com>
2023-09-24 01:06:54 +0900

    feat: 채팅방 경로(url) 생성
    - sidebar를 사용하기 위해 (home)/page 내용을 복사해 사용하며, 추후 리펙토링 필요합니다

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,5 +1,23 @@
+import SideBar from "@/components/SideBar";
+import { useParams } from "next/navigation";
+
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const { id } = useParams();
+  const selectedChatId = typeof id === "string" ? id : undefined;
+
+  return (
+    <main className="grid w-full grid-cols-8">
+      <div className="col-span-2">
+        <SideBar selectedChatId={selectedChatId} />
+      </div>
+
+      <div className="col-span-6 flex justify-center h-screen">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          initail page
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ChatPage;
